3b91d748f955d5738051e9ece4837ece80a63bf6
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2017-06-03 21:05:31 +0200

    Fix ids so it is easier to refer to them in tests

--- src/main/resources/db/test-data/V3__initial.sql
+++ src/main/resources/db/test-data/V3__initial.sql
@@ -6,6 +6,6 @@ INSERT INTO club (id, name) VALUES(4, 'Greve BTK');
 INSERT INTO player (id, name, version) VALUES(1, 'Jan', 1);
 INSERT INTO player (id, name, version) VALUES(2, 'Samsonov', 1);
 
-INSERT INTO club_membership (club_id, player_id, member_from) VALUES(1, 1, CURRENT_DATE);
-INSERT INTO club_membership (club_id, player_id, member_from) VALUES(1, 2, CURRENT_DATE);
-INSERT INTO club_membership (club_id, player_id, member_from) VALUES(2, 2, CURRENT_DATE);
+INSERT INTO club_membership (id, club_id, player_id, member_from) VALUES(1, 1, 1, CURRENT_DATE);
+INSERT INTO club_membership (id, club_id, player_id, member_from) VALUES(2, 1, 2, CURRENT_DATE);
+INSERT INTO club_membership (id, club_id, player_id, member_from) VALUES(3, 2, 2, CURRENT_DATE);
