aeb18e8eadd4e39a774823a1a328729e219c267b
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2017-06-06 21:54:39 +0200

    Externalize configuration with @ConfigurationProperties

---
+++ src/main/java/dk/bringlarsen/bportal/configuration/ApplicationProperties.java
@@ -0,0 +1,68 @@
+package dk.bringlarsen.bportal.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Validated
+@ConfigurationProperties(prefix = "dk.bringlarsen")
+public class ApplicationProperties {
+
+    @NotNull
+    private Integer port;
+
+    @NotNull
+    private String username;
+
+    @Valid
+    private Db db;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Db getDb() {
+        return db;
+    }
+
+    public void setDb(Db db) {
+        this.db = db;
+    }
+
+    public static class Db {
+        @NotNull
+        private String username;
+        @NotNull
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
