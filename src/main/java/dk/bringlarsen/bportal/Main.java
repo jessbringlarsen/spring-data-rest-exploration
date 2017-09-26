7aee6c233877d61c691443ff2385b43401511044
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2017-09-26 21:18:25 +0200

    Disable unused elements

--- src/main/java/dk/bringlarsen/bportal/Main.java
+++ src/main/java/dk/bringlarsen/bportal/Main.java
@@ -3,17 +3,18 @@ package dk.bringlarsen.bportal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import javax.swing.*;
-
 @EnableAsync
-@SpringBootApplication
+// We are not using eg. websocket so we disable support for it.
+@SpringBootApplication(exclude = { WebSocketAutoConfiguration.class, JmxAutoConfiguration.class})
 public class Main {
 
     @Autowired
-    ApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
