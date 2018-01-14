e0dc7864046ea0e7030e1f6f9ff9331ecca58db4
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2018-01-14 22:35:15 +0100

    Manually created a paging enabled controller

--- src/main/java/dk/bringlarsen/bportal/controller/MatchController.java
+++ src/main/java/dk/bringlarsen/bportal/controller/MatchController.java
@@ -6,27 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Resources;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RepositoryRestController
 public class MatchController {
 
     private final MatchJpaRepository repository;
+    private final PagedResourcesAssembler assembler;
 
     @Autowired
-    public MatchController(MatchJpaRepository repository) {
+    public MatchController(MatchJpaRepository repository, PagedResourcesAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/matches")
-    public @ResponseBody ResponseEntity<?> getMatches(Pageable pageable) {
+    public HttpEntity<PagedResources<Match>> getMatches(Pageable pageable) {
         Page<Match> page = repository.findAll(pageable);
-        Resources<Match> resources = new Resources<>(page);
 
-        return ResponseEntity.ok(resources);
+        return new ResponseEntity(assembler.toResource(page), HttpStatus.OK);
     }
 }
--- src/main/resources/db/test-data/V3__initial.sql
+++ src/main/resources/db/test-data/V3__initial.sql
@@ -5,8 +5,17 @@ INSERT INTO club (id, name) VALUES(4, 'Greve BTK');
 
 INSERT INTO player (id, name, version) VALUES(1, 'Jan', CURRENT_DATE);
 INSERT INTO player (id, name, version) VALUES(2, 'Samsonov', CURRENT_DATE);
+INSERT INTO player (id, name, version) VALUES(3, 'Ohh Sang Eun', CURRENT_DATE);
+INSERT INTO player (id, name, version) VALUES(4, 'Maze', CURRENT_DATE);
+INSERT INTO player (id, name, version) VALUES(5, 'Monrad', CURRENT_DATE);
+INSERT INTO player (id, name, version) VALUES(6, 'Applegreen', CURRENT_DATE);
 
 INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(1, CURRENT_DATE, 1, 2);
+INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(2, CURRENT_DATE, 3, 4);
+INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(3, CURRENT_DATE, 5, 6);
+INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(4, CURRENT_DATE, 1, 3);
+INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(5, CURRENT_DATE, 4, 5);
+INSERT INTO match_table (id, when_column, player1_id, player2_id) VALUES(6, CURRENT_DATE, 6, 1);
 
 INSERT INTO club_membership (id, club_id, player_id, member_from) VALUES(1, 1, 1, CURRENT_DATE);
 INSERT INTO club_membership (id, club_id, player_id, member_from) VALUES(2, 1, 2, CURRENT_DATE);
